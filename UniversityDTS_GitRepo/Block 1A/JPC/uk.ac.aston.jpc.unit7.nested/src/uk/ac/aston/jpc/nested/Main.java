12b45cdd4b25ae89a97b9911388166cc4f50546e
Bradley Willies <bradley.willies@gmail.com>
2021-05-05 19:18:31 +0100

    JPC unit7 lab - completed step 4

--- UniversityDTS_GitRepo/Block 1A/JPC/uk.ac.aston.jpc.unit7.nested/src/uk/ac/aston/jpc/nested/Main.java
+++ UniversityDTS_GitRepo/Block 1A/JPC/uk.ac.aston.jpc.unit7.nested/src/uk/ac/aston/jpc/nested/Main.java
@@ -2,13 +2,27 @@ package uk.ac.aston.jpc.nested;
 
 import static uk.ac.aston.jpc.nested.v4.SteppingRange.range;
 
+import java.util.function.Consumer;
+
 public class Main {
 
 	public static void main(String[] args) {
 		SizedIterable<Integer> r = range(5, 5, 101);
-		for (int x : r) {
-			System.out.println(x);
-		}
+		
+//		Consumer<Integer> c = new Consumer<Integer>() {
+//			@Override
+//			public void accept(Integer t) {
+//				System.out.println(t);
+//			}
+//		};
+		
+//		Consumer<Integer> c = t -> System.out.println(t);
+		
+		r.forEach(System.out::println);
+		
+//		for (int x : r) {
+//			System.out.println(x);
+//		}
 		System.out.println("There are " + r.size() + " numbers.");
 	}
 
