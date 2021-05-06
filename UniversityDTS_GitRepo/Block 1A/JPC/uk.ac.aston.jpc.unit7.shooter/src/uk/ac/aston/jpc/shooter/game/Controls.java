e6480aa961ab969a8899dff31b4d8ea9d0e051ec
Bradley Willies <bradley.willies@gmail.com>
2021-05-06 13:30:39 +0100

    JPC unit7 lab - completed step 8

--- UniversityDTS_GitRepo/Block 1A/JPC/uk.ac.aston.jpc.unit7.shooter/src/uk/ac/aston/jpc/shooter/game/Controls.java
+++ UniversityDTS_GitRepo/Block 1A/JPC/uk.ac.aston.jpc.unit7.shooter/src/uk/ac/aston/jpc/shooter/game/Controls.java
@@ -2,9 +2,14 @@ package uk.ac.aston.jpc.shooter.game;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 
 /**
  * Keeps track of which actions are being currently requested by the player in
@@ -31,12 +36,86 @@ public class Controls {
 
 	private static final Map<KeyCombination, Integer> KEY_TO_ACTION = new HashMap<>();
 
-	// TODO add static initialisation block
+	static {
+		// WASD
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.W), ACTION_UP);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.A), ACTION_LEFT);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.S), ACTION_DOWN);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.D), ACTION_RIGHT);
+		
+		// Arrow Keys
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.UP), ACTION_UP);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.LEFT), ACTION_LEFT);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.DOWN), ACTION_DOWN);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.RIGHT), ACTION_RIGHT);
+		
+		// Num pad
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.KP_UP), ACTION_UP);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.KP_LEFT), ACTION_LEFT);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.KP_DOWN), ACTION_DOWN);
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.KP_RIGHT), ACTION_RIGHT);
+		
+		// Spacebar
+		KEY_TO_ACTION.put(new KeyCodeCombination(KeyCode.SPACE), ACTION_FIRE);
+	}
 
 	private final Map<Integer, Boolean> actionActive = new HashMap<>();
 
 	public Controls(Scene scene) {
-		// TODO need to set up key handlers here with a local class
+		// V1 ---> 
+		/*
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent ev) {
+				for (Entry<KeyCombination, Integer> entry : KEY_TO_ACTION.entrySet()) {
+					final KeyCombination kc = entry.getKey();
+					final int action = entry.getValue();
+					if (kc.match(ev)) {
+						actionActive.put(action, true);
+						break;
+					}
+				}
+			}
+		});
+		
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent ev) {
+				for (Entry<KeyCombination, Integer> entry : KEY_TO_ACTION.entrySet()) {
+					final KeyCombination kc = entry.getKey();
+					final int action = entry.getValue();
+					if (kc.match(ev)) {
+						actionActive.put(action, false);
+						break;
+					}
+				}
+			}
+		});
+		*/
+		
+		// V2 --->
+		class KeyMatchingHandler implements EventHandler<KeyEvent> {
+			private boolean pressOrRelease;
+			
+			public KeyMatchingHandler(boolean pressOrRelease) {
+				this.pressOrRelease = pressOrRelease;
+			}
+			
+			@Override
+			public void handle(KeyEvent ev) {
+				for (Entry<KeyCombination, Integer> entry : KEY_TO_ACTION.entrySet()) {
+					final KeyCombination kc = entry.getKey();
+					final int action = entry.getValue();
+					if (kc.match(ev)) {
+						actionActive.put(action, pressOrRelease);
+						break;
+					}
+				}
+			}
+		}
+		
+		scene.setOnKeyPressed(new KeyMatchingHandler(true));
+		scene.setOnKeyReleased(new KeyMatchingHandler(false));
 	}
 
 	/**
