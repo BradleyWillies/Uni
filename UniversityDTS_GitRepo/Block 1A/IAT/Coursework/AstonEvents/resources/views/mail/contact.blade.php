6ed235e4d88ae2cd1ec4ec9b0f7aed2db8696884
Bradley Willies <bradley.willies@gmail.com>
2021-06-12 16:55:52 +0100

    IAT Coursework - added organiser can link an event to another event and website

--- UniversityDTS_GitRepo/Block 1A/IAT/Coursework/AstonEvents/resources/views/mail/contact.blade.php
+++
@@ -1,9 +0,0 @@
-@component('mail::message')
-    ### {{ $name }} sent you a message regarding event: {{ $eventName }}
-
-    {{ $message }}
-
-    Sender's email: {{ $email }}
-
-    - {{ config('app.name') }} -
-@endcomponent
